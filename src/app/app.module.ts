d6376df45c8d40eaf9f54ca0b57fa91b1ff06e29
PrateekaMisra <prateeka.misra@gmail.com>
2022-02-15 12:58:17 +0530

    added formModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -25,6 +25,7 @@ import { PostService } from './services/post.service';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [UserService, PostService],
